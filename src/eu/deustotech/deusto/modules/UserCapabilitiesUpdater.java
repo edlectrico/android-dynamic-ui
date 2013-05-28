e20524c0ae4022171fd1d29cd5e171bdf31fa3e5
Eduardo Castillejo <eduardo.castillejo@deusto.es>
2013-05-28 11:16:01 +0200

    Added UserCapabilitiesUpdater

---
+++ src/eu/deustotech/deusto/modules/UserCapabilitiesUpdater.java
@@ -0,0 +1,20 @@
+package eu.deustotech.deusto.modules;
+
+import eu.deustotech.deusto.model.ICapability;
+
+public class UserCapabilitiesUpdater {
+
+	/**
+	 * This module will be the responsible for updating the user
+	 * capabilities taking into account the current context
+	 * parameters. Therefore, it should modify current
+	 * user capabilities taking into account the new current
+	 * context situation. 
+	 */
+	
+	static public ICapability update(ICapability user, ICapability context){
+		
+		return null;
+	}
+	
+}
