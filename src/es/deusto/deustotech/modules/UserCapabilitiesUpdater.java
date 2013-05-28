f7be31d6debf466c06a9dd66bb0923ba3c7a66d9
Eduardo Castillejo <eduardo.castillejo@deusto.es>
2013-05-28 14:36:00 +0200

    Added sound user update

--- src/es/deusto/deustotech/modules/UserCapabilitiesUpdater.java
+++ src/es/deusto/deustotech/modules/UserCapabilitiesUpdater.java
@@ -6,32 +6,47 @@ import es.deusto.deustotech.model.ICapability.CAPABILITY;
 public class UserCapabilitiesUpdater {
 
 	/**
-	 * This module will be the responsible for updating the user
-	 * capabilities taking into account the current context
-	 * parameters. Therefore, it should modify current
-	 * user capabilities taking into account the new current
-	 * context situation. 
+	 * This module will be the responsible for updating the user capabilities
+	 * taking into account the current context parameters. Therefore, it should
+	 * modify current user capabilities taking into account the new current
+	 * context situation.
 	 */
-	
-	static public ICapability update(ICapability user, ICapability context){
-		//TODO: for the moment just adapt noise and lightning
 
-		//TODO: Try to cover any possible case
-		//if user.CAPABILITY.USER_BRIGHTNESS == DEFAULT | LOW | HIGH and context.CAPABILITY.CONTEXT_LIGHTNING
-		//then newCapability = USER_BRIGHTNESS.VERY_HIGH for avoiding sun reflection
-		
+	static public ICapability update(ICapability user, ICapability context) {
+		// TODO: for the moment just adapt noise and lightning
+
+		// TODO: Try to cover any possible case
+		// if user.CAPABILITY.USER_BRIGHTNESS == DEFAULT | LOW | HIGH and
+		// context.CAPABILITY.CONTEXT_LIGHTNING
+		// then newCapability = USER_BRIGHTNESS.VERY_HIGH for avoiding sun
+		// reflection
+
 		final Object userBrightnessValue 		= user.getCapabilityValue(CAPABILITY.USER_BRIGHTNESS);
 		final Object contextIlluminanceValue 	= context.getCapabilityValue(CAPABILITY.CONTEXT_LIGHTNING);
-		
-		if ((userBrightnessValue.equals(ICapability.BRIGHTNESS.DEFAULT) || 
-				userBrightnessValue.equals(ICapability.BRIGHTNESS.LOW)  ||
-				userBrightnessValue.equals(ICapability.BRIGHTNESS.HIGH))){
-			if (contextIlluminanceValue.equals(ICapability.ILLUMINANCE.SUNLIGHT)){
-				user.setCapabilityValue(CAPABILITY.USER_BRIGHTNESS, ICapability.BRIGHTNESS.VERY_HIGH);
+
+		if ((userBrightnessValue.equals(ICapability.BRIGHTNESS.DEFAULT)
+				|| userBrightnessValue.equals(ICapability.BRIGHTNESS.LOW)
+				|| userBrightnessValue.equals(ICapability.BRIGHTNESS.HIGH))) {
+			if (contextIlluminanceValue
+					.equals(ICapability.ILLUMINANCE.SUNLIGHT)) {
+				user.setCapabilityValue(CAPABILITY.USER_BRIGHTNESS,
+						ICapability.BRIGHTNESS.VERY_HIGH);
 			}
 		}
-		
+
+		final Object userVolumeValue 	= user.getCapabilityValue(CAPABILITY.USER_VOLUME);
+		final float contextNoiseValue 	= (Float) context.getCapabilityValue(CAPABILITY.CONTEXT_NOISE);
+
+		if ((userVolumeValue.equals(ICapability.VOLUME.DEFAULT))
+				|| (userVolumeValue.equals(ICapability.VOLUME.LOW))
+				|| (userVolumeValue.equals(ICapability.VOLUME.HIGHT))) {
+			if (contextNoiseValue > 90F) {
+				user.setCapabilityValue(CAPABILITY.USER_VOLUME,
+						ICapability.VOLUME.VERY_HIGH);
+			}
+		}
+
 		return user;
 	}
-	
+
 }
